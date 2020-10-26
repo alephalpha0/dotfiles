5a5f6f0b5f7d8ce91ebe3aa56831d26ad57a98a8
alephalpha0 <alephalpha0@rootofpi.me>
2020-10-25 20:10:11 -0500

    update to liquidpromptrc, liquidpromopt.aa0.ps1. brought the prompt down to two lines instead of three. trying to figure out where the phantom newline is creeping in between executions.

--- .config/zsh/liquidprompt.aa0.ps1
+++ .config/zsh/liquidprompt.aa0.ps1
@@ -24,14 +24,14 @@
 # add permissions colon, pwd, venv, proxy, job runtime, error code
 LP_PS1="${LP_PS1_PREFIX}${LP_BRACKET_OPEN}${LP_PERM}"
 LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
-LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}"
+LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}"
 # Add time and VCS infos
 # If root, the info has not been collected unless LP_ENABLE_VCS_ROOT
 # is set.
-LP_PS1="${LP_PS1}${LP_MARK_PREFIX}${LP_TIME}${LP_VCS}"
+LP_PS1="${LP_PS1}${LP_TIME}${LP_VCS}"
 
 # add prompt mark
-LP_PS1="${LP_PS1}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
+LP_PS1="${LP_PS1}${LP_MARK}${LP_PS1_POSTFIX}"
 
 # "invisible" parts
 # Get the current prompt on the fly and make it a title
